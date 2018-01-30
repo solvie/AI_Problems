d9b423f79bf531f389eb270f845ee2146c95d6ea
Solvie <lee.solvie@gmail.com>
2018-01-30 13:29:45 -0500

    bug fixed

--- Optimization.py
+++ Optimization.py
@@ -12,7 +12,7 @@ def applyEquation(x):
 	return math.sin(x*x/2.0)/math.log(x+4,2)
 
 def findMaxReturnIndex(arrayOfTuples):
-	maxvar = arrayOfTuples[0][0]
+	maxvar = arrayOfTuples[0][1]
 	maxtup = arrayOfTuples[0]
 	for tup in arrayOfTuples:
 		if tup[1] > maxvar:
@@ -33,8 +33,8 @@ def TwoDHillClimbing(step_size, startPoint, xmin, xmax):
 		xright = x+step_size
 		if xright <=xmax:
 			neighbors.append((xright,applyEquation(xright)))
-		bestSuccessor = findMaxReturnIndex(neighbors)
 		y = applyEquation(x)
+		bestSuccessor = findMaxReturnIndex(neighbors)
 		if bestSuccessor[1] <= y:
 			return (round(x,2),round(y, 5), numsteps) #index, convergence value, numsteps
 		x = bestSuccessor[0]		
@@ -64,20 +64,20 @@ plt.title('Graph of function y = sin(x^2/2)/log_2(x+4)')
 #plt.savefig('functiongraph.svg')
 
 
-startindex=0
+currentIndex=0
 currentStep=0.01
 motherArray=[] #motherArray[i] indicates values for starting point = i
-while startindex <=10:
+while currentIndex <=10:
 	subArray = []
-	while currentStep <=0.1:
-		subArray.append(TwoDHillClimbing(currentStep, startindex,0,10))
+	while currentStep <=0.1:#tab me
+		subArray.append(TwoDHillClimbing(currentStep, currentIndex,0,10))
 		currentStep+=0.01
-	startindex+=1
+	currentIndex+=1
 	currentStep=0.01
-	motherArray.append(subArray)
-	print(startindex)
+	motherArray.append(subArray)#tab me
+print(currentIndex)
 print('motherarray 0')
-print(motherArray[2])
+print(motherArray[1])
 
 #plt.figure()
 plt.plot(xvals,yvals)
