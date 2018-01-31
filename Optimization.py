70dc1bc2ccb71dd37298bc08dfcb4e0f7579d332
Solvie <lee.solvie@gmail.com>
2018-01-30 22:39:02 -0500

    atrociously messy but does the job

--- Optimization.py
+++ Optimization.py
@@ -8,8 +8,12 @@
 
 import math
 import random
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import csv
+import seaborn as sns
+import pandas as pd
+import numpy as np
+ 
 
 def applyEquation(x):
 	return math.sin(x*x/2.0)/math.log(x+4,2)
@@ -89,7 +93,7 @@ def simulatedAnnealing(step_size, startPoint, xmin, xmax, temperature):
 				y = randomNeighbor[1]		
 	return ((round(x,2),round(y, 5)), (step_size,numsteps))
 
-def writeCompleteCsvTable(title, motherarray):
+def writeCompleteCsvTableHillCli(title, motherarray):
 	with open(title,'w') as csvfile:
 		fieldnames=['Start index', 'Step size', 'x', 'y', 'Steps to convergence']
 		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -98,6 +102,17 @@ def writeCompleteCsvTable(title, motherarray):
 			childarray = motherarray[i]
 			for positionTuple, stepTuple in childarray:
 				writer.writerow({'Start index': i ,'Step size': stepTuple[0] ,'x': positionTuple[0], 'y': positionTuple[1], 'Steps to convergence': stepTuple[1]})
+
+def writeCompleteCsvTableSimAnn(title, motherarray):
+	with open(title,'w') as csvfile:
+		fieldnames=['Start index', 'Step size', 'x', 'y', 'Steps to convergence']
+		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+		writer.writeheader()
+		for i in range (0,len(motherarray)):
+			childarray = motherarray[i]
+			for positionTuple, stepTuple in childarray:
+				writer.writerow({'Start index': i ,'Step size': stepTuple[0] ,'x': positionTuple[0], 'y': positionTuple[1], 'Steps to convergence': stepTuple[1]})
+
 
 def saveFunctionPlot():
 	xvals=[]
@@ -118,7 +133,7 @@ def saveFunctionPlot():
 	plt.title('Graph of function y = sin(x^2/2)/log_2(x+4)')
 	plt.savefig('functiongraph.svg')
 
-def hillClimbingResults():
+def hillClimbingResults(writeCSV):
 	currentIndex=0
 	currentStep=0.01
 	motherArray=[] #motherArray[i] indicates values for starting point = i
@@ -130,11 +145,12 @@ def hillClimbingResults():
 		currentIndex+=1
 		currentStep=0.01
 		motherArray.append(subArray)
-	print('motherarray')
-	print(motherArray)
-	writeCompleteCsvTable('allresults_hill-climbing.csv', motherArray)
+	if writeCSV:
+		writeCompleteCsvTable('allresults_hill-climbing.csv', motherArray)
+	return motherArray
 
-def simulatedAnnealingResults(temperatures):
+
+def simulatedAnnealingResults(temperatures, generateCSV):
 	currentStep=0.02
 	motherArray=[] 
 	for temperature in temperatures:
@@ -144,5 +160,54 @@ def simulatedAnnealingResults(temperatures):
 		motherArray.append(subArray)
 	return motherArray
 # simulatedAnnealingResults([0.01,0.1, 1,5,10,20])
-apple = simulatedAnnealingResults([1,5,10])
+#apple = simulatedAnnealingResults([1,5,10])
+
+def findMaxConvergeValuesForEach(array): #prints in latex format
+	for index in range (0,10):
+		maxtup = array[index][0][0]
+		maxval = array[index][0][0][1] #y val
+		for positionTuple, stepTuple in array[index]:
+			if positionTuple[1]>maxval:
+				maxval = positionTuple[1]
+				maxtup = positionTuple
+		print(str(index) + " & " + str(maxtup[0]) + " & " + str(maxtup[1])+"\\\\")
+
+def findUniqueVals(inputarray):
+	retarray = []
+	for array in inputarray:
+		for tuptup in array:
+			if tuptup[0] not in retarray:
+				print('adding')
+				retarray.append(tuptup[0])
+	retarray.sort(key=lambda tup: tup[0])
+	return retarray
+
+def generateHeatmapTableSimAnn(temperatures, resultsarray):
+	with open('heatmap','w') as csvfile:
+		fieldnames=['Temperature']
+		vals = findUniqueVals(resultsarray)
+		for i in range (0, len(vals)):
+			fieldnames.append(str(vals[i]))
+		print(fieldnames)
+
+		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+		writer.writeheader()
+		i =0
+		for temp in temperatures:
+			dictionary={}
+			dictionary['Temperature']= temp
+			for val in vals:#initialize dictionary
+				dictionary[str(val)] = 0
+			childarray = resultsarray[i]
+			for positionTuple, stepTuple in childarray:
+				for val in vals:
+					if positionTuple[1]==val[1]:
+						dictionary[str(val)] = dictionary[str(val)]+1
+			print(temp)
+			print(dictionary)
+			writer.writerow(dictionary)
+			i+=1
+
+
+
 
