af7ff00156ab7657e236f355b1536ee28d9e4d37
Solvie <lee.solvie@gmail.com>
2018-01-30 19:45:22 -0500

    A bit of cleanup

--- Optimization.py
+++ Optimization.py
@@ -1,5 +1,6 @@
 # Optimization - Hill-climbing and Simulated Annealing
 #
+# The function in question:
 # y = sin(x^2/2)/log_2(x+4)
 #
 # Solvie Lee 
@@ -24,7 +25,7 @@ def findMaxReturnIndex(arrayOfTuples):
 
 
 #x min and xmax constrains the search to a certain domain
-def TwoDHillClimbing(step_size, startPoint, xmin, xmax, ):
+def twoDHillClimbing(step_size, startPoint, xmin, xmax, ):
 	x = startPoint
 	numsteps = 0
 	while True:
@@ -45,14 +46,18 @@ def TwoDHillClimbing(step_size, startPoint, xmin, xmax, ):
 	print('something went wrong')
 	return None
 
-def simulatedAnnealing(step_size, startPoint, xmin, xmax, temperature, runtime):
+def simulatedAnnealing(step_size, startPoint, xmin, xmax, temperature):
 	x = startPoint
 	numsteps = 0
 	time = 0
-	while time < runtime:
-		temperature = 0.999*temperature
-		# print('temperature')
-		# print(temperature)
+	frozen = False
+	steady = False
+	steadyval = None
+	steadycount = 0
+	while True:
+		temperature = 0.9999*temperature
+		if round(temperature, 5)==0:
+			frozen = True
 		time+=1
 		randomNeighbor = None
 		neighbors = []
@@ -67,21 +72,21 @@ def simulatedAnnealing(step_size, startPoint, xmin, xmax, temperature, runtime):
 		else:
 			randomNeighbor = neighbors[int(round(random.uniform(0,1),0))]
 		y = applyEquation(x)
-		# if time <200 or time%500==0:
-			# print('temp&the math')
-			# print(temperature)
-			# print((-y+randomNeighbor[1]))
-			# print(math.exp(-abs(y-randomNeighbor[1])/temperature))
-		if randomNeighbor[1] > y or random.uniform(0,1)<math.exp((-y+randomNeighbor[1])/temperature):
-			# print('CHOOSING TO MOVE')
-			# print((-y+randomNeighbor[1]))
-			# print(math.exp(-abs(y-randomNeighbor[1])/temperature))
+		numsteps+=1
+		if randomNeighbor[1] > y:
 			x = randomNeighbor[0]
 			y = randomNeighbor[1]
 		else:
-			# print('nah we aint picking this this')
-			continue	
-		numsteps+=1
+			if steadyval ==y:
+				steadycount+=1
+			else:
+				steadyval = y
+				steadycount=0
+			if steadycount>100 and frozen:
+				break
+			if random.uniform(0,1)<math.exp((-y+randomNeighbor[1])/temperature):
+				x = randomNeighbor[0]
+				y = randomNeighbor[1]		
 	return ((round(x,2),round(y, 5)), (step_size,numsteps))
 
 def writeCompleteCsvTable(title, motherarray):
@@ -94,67 +99,50 @@ def writeCompleteCsvTable(title, motherarray):
 			for positionTuple, stepTuple in childarray:
 				writer.writerow({'Start index': i ,'Step size': stepTuple[0] ,'x': positionTuple[0], 'y': positionTuple[1], 'Steps to convergence': stepTuple[1]})
 
+def saveFunctionPlot():
+	xvals=[]
+	yvals=[]
+	step_size = 0.01
+	i=0
+	x=0
+	while x <= 10:
+		x = i*step_size
+		xvals.append(x)
+		yvals.append(applyEquation(x))
+		i+=1
+	plt.figure()
+	plt.plot(xvals,yvals)
+	plt.axis([0, 10, -1, 1])
+	plt.xlabel('x')
+	plt.ylabel('y')
+	plt.title('Graph of function y = sin(x^2/2)/log_2(x+4)')
+	plt.savefig('functiongraph.svg')
 
-xvals=[]
-yvals=[]
-step_size = 0.01
-i=0
-x=0
+def hillClimbingResults():
+	currentIndex=0
+	currentStep=0.01
+	motherArray=[] #motherArray[i] indicates values for starting point = i
+	while currentIndex <=10:
+		subArray = []
+		while currentStep <=0.1:
+			subArray.append(twoDHillClimbing(currentStep, currentIndex,0,10))
+			currentStep+=0.01
+		currentIndex+=1
+		currentStep=0.01
+		motherArray.append(subArray)
+	print('motherarray')
+	print(motherArray)
+	writeCompleteCsvTable('allresults_hill-climbing.csv', motherArray)
 
-while x <= 10:
-	x = i*step_size
-	xvals.append(x)
-	yvals.append(applyEquation(x))
-	i+=1
-
-#plt.figure()
-#plt.plot(xvals,yvals)
-plt.axis([0, 10, -1, 1])
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Graph of function y = sin(x^2/2)/log_2(x+4)')
-#plt.savefig('functiongraph.svg')
-
-
-# currentIndex=0
-# currentStep=0.01
-# motherArray=[] #motherArray[i] indicates values for starting point = i
-# while currentIndex <=10:
-# 	subArray = []
-# 	while currentStep <=0.1:
-# 		subArray.append(TwoDHillClimbing(currentStep, currentIndex,0,10))
-# 		currentStep+=0.01
-# 	currentIndex+=1
-# 	currentStep=0.01
-# 	motherArray.append(subArray)
-# print('motherarray')
-# print(motherArray)
-# writeCompleteCsvTable('allresults_hill-climbing.csv', motherArray)
-
-
-currentIndex=0
-currentStep=0.05
-motherArray=[] #motherArray[i] indicates values for starting point = i
-# while currentIndex <=10:
-subArray = []
-for temperature in [0.03,1,5,10]:#,1, 5,10]:#,1,1000,1000000,1000000000]:#,0.03,0.04]:
-	subArray = []
-	print('temp')
-	print(temperature)
-	for iteration in range (1,10):
-		subArray.append(simulatedAnnealing(currentStep, 5,0,10, temperature, 1000000))
-	# currentIndex+=1
-	# currentStep=0.01
-	print(subArray)
-motherArray.append(subArray)
-print('motherarray')
-# print(motherArray)
-
-
-#plt.figure()
-plt.plot(xvals,yvals)
-plt.xlabel('Steps to convergence')
-plt.ylabel('y')
-plt.title('Graph starting at 0')
-
+def simulatedAnnealingResults(temperatures):
+	currentStep=0.02
+	motherArray=[] 
+	for temperature in temperatures:
+		subArray = []
+		for iteration in range (1,10):
+			subArray.append(simulatedAnnealing(currentStep, 5,0,10, temperature))
+		motherArray.append(subArray)
+	return motherArray
+# simulatedAnnealingResults([0.01,0.1, 1,5,10,20])
+apple = simulatedAnnealingResults([1,5,10])
 
